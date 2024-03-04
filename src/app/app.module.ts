c494203b22a22d7c81edd94e9d4a3382d98e1f8c
Hoang Thai <hoangthainnbt.0503@gmail.com>
2024-03-04 22:02:59 +0700

    commit lan dau

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 
 const COMPONENT = [
+  FooterComponent,
+  HeaderComponent,
+  MainComponent
 ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...COMPONENT
   ],
   imports: [
     BrowserModule,
